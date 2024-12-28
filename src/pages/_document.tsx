867087bf3f5e2f4d0c0af6c46aa3a47da1301e82
Ulysses Queiroz <ulyssesq@gmail.com>
2024-12-28 17:52:53 -0300

    chore: adicionando defer no gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script dangerouslySetInnerHTML={{
+        <script 
+        defer
+        dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
